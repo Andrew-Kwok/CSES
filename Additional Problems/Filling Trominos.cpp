197dff143aba2d1abfaa6159e5e2391bcc0d52df
Andrew Kwok (Arch) <andrewkok80@gmail.com>
2023-11-23 17:21:55 -0500

    add solutions

--- Additional Problems/Filling Trominos.cpp
+++ Additional Problems/Filling Trominos.cpp
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define pb push_back
 #define mp make_pair
 #define f1 first
 #define s2 second
-
+ 
 #define fastio ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define debug(x) cerr << "[" << #x << "]: " << x << "\n";
-
+ 
 using ll = long long;
 using ld = long double;
 using ii = pair<int, int>;
 using pl = pair<ll, ll>;
-
+ 
 const ld PI = 4*atan((ld)1);
 const int MAX = 107;
 const int dx[] = { 1, -1, 0, 0 };
 const int dy[] = { 0, 0, 1, -1 };
-
+ 
 int n, m;
-bool dp[MAX][MAX];
 char res[MAX][MAX] = {};
-
+ 
 void connect(int x1, int y1, int x2, int y2, int x3, int y3)
 {
 	if (res[x1][y1] || res[x2][y2] || res[x3][y3])
 		return;
-
+ 
 	set<char> st;
 	for (int i = 0; i < 4; ++i)
 	{
@@ -47,14 +46,14 @@ void connect(int x1, int y1, int x2, int y2, int x3, int y3)
 		if (res[nx][ny])
 			st.insert(res[nx][ny]);
 	}
-
+ 
 	char cur = 'A';
 	while (st.count(cur))
 		cur++;
-
+ 
 	res[x1][y1] = res[x2][y2] = res[x3][y3] = cur;
 }
-
+ 
 void place(int r1, int c1, int r2, int c2)
 {
 	if ((r2 - r1 + 1) % 2 == 0 && (c2 - c1 + 1) % 3 == 0)
@@ -85,7 +84,7 @@ void place(int r1, int c1, int r2, int c2)
 		}
 	}
 }
-
+ 
 const string nine[] = {
 	"AABBCAABB",
 	"ACCBCCABA",
@@ -93,7 +92,7 @@ const string nine[] = {
 	"BABACBABB",
 	"AABBCCAAB"
 };
-
+ 
 void place9(int r1, int c1, int r2, int c2)
 {
 	if ((r2 - r1 + 1) % 5 == 0)
@@ -108,7 +107,7 @@ void place9(int r1, int c1, int r2, int c2)
 					{
 						vector<ii> v;
 						v.pb({i+r, j+c});
-
+ 
 						for (int k = 0; k < 4; ++k)
 						{
 							int nr = r + dx[k];
@@ -116,7 +115,7 @@ void place9(int r1, int c1, int r2, int c2)
 							if (0 <= nr && nr < 5 && 0 <= nc && nc < 9 && nine[r][c] == nine[nr][nc])
 								v.pb({i+nr, j+nc});
 						}
-
+ 
 						if (v.size() == 3)
 							connect(v[0].f1, v[0].s2, v[1].f1, v[1].s2, v[2].f1, v[2].s2);
 					}
@@ -136,7 +135,7 @@ void place9(int r1, int c1, int r2, int c2)
 					{
 						vector<ii> v;
 						v.pb({i+r, j+c});
-
+ 
 						for (int k = 0; k < 4; ++k)
 						{
 							int nr = r + dx[k];
@@ -144,7 +143,7 @@ void place9(int r1, int c1, int r2, int c2)
 							if (0 <= nr && nr < 9 && 0 <= nc && nc < 5 && nine[c][r] == nine[nc][nr])
 								v.pb({i+nr, j+nc});
 						}
-
+ 
 						if (v.size() == 3)
 							connect(v[0].f1, v[0].s2, v[1].f1, v[1].s2, v[2].f1, v[2].s2);
 					}
@@ -153,133 +152,101 @@ void place9(int r1, int c1, int r2, int c2)
 		}
 	}
 }
-
+ 
 void solve()
 {
-	for (int i = 0; i < MAX; ++i)
-		dp[i][0] = dp[0][i] = true;
-
-	for (int i = 1; i < MAX; ++i)
-	{		
-		for (int j = 1; j < MAX; ++j)
-		{
-			if (j % 3 == 0 && i >= 2)
-				dp[i][j] |= dp[i-2][j];
-			if (j % 2 == 0 && i >= 3)
-				dp[i][j] |= dp[i-3][j];
-			if (i % 3 == 0 && j >= 2)
-				dp[i][j] |= dp[i][j-2];
-			if (i % 2 == 0 && j >= 3)
-				dp[i][j] |= dp[i][j-3];
-			if (i % 9 == 0 && j >= 5)
-				dp[i][j] |= dp[i][j-5];
-			if (i >= 5 && j % 9 == 0)
-				dp[i][j] |= dp[i-5][j];
-		}
-	}
-
+	memset(res, 0, sizeof(res));
+	
 	cin >> n >> m;
-	if (!dp[n][m])
+	if (n == 1 || m == 1)
 	{
 		cout << "NO\n";
 		return;
 	}
 
-	memset(res, 0, sizeof(res));
-	for (int i = n, j = m; i > 0 && j > 0;)
+	if (n % 3 && m % 3)
 	{
-		if (j % 3 == 0 && i >= 2 && dp[i-2][j])
-		{
-			place(i-1, 1, i, j);
-			i -= 2;
-		}
-		else if (j % 2 == 0 && i >= 3 && dp[i-3][j])
-		{
-			place(i-2, 1, i, j);
-			i -= 3;
-		}
-		else if (i % 3 == 0 && j >= 2 && dp[i][j-2])
-		{
-			place(1, j-1, i, j);
-			j -= 2;
-		}
-		else if (i % 2 == 0 && j >= 3 && dp[i][j-3])
-		{
-			place(1, j-2, i, j);
-			j -= 3;
-		}
-		else if (i >= 9 && j >= 5 && dp[i-9][j-5])
-		{
-			place9(1, j-4, i, j);
-			j -= 5;
-		}
-		else if (i >= 5 && j >= 9 && dp[i-5][j-9])
-		{
-			place9(i-4, 1, i, j);
-			i -= 5;
-		}
-
-		// if (ctr == 4)
-		// 	break;
+		cout << "NO\n";
+		return;
 	}
 
-	// bool vst[MAX][MAX] = {};
-	// for (int i = 1; i <= n; ++i)
-	// {
-	// 	for (int j = 1; j <= m; ++j) if (!vst[i][j])
-	// 	{
-	// 		int ctr = 1;
-	// 		queue<ii> q;
-	// 		vst[i][j] = true;
-	// 		q.push({i, j});
-
-	// 		while (!q.empty())
-	// 		{
-	// 			auto [r, c] = q.front(); q.pop();
-	// 			for (int k = 0; k < 4; ++k)
-	// 			{
-	// 				int nr = r + dx[k];
-	// 				int nc = c + dy[k];
-
-	// 				if (1 <= nr && nr <= n && 1 <= nc && nc <= m && !vst[nr][nc] && res[r][c] == res[nr][nc])
-	// 				{
-	// 					vst[nr][nc] = true;
-	// 					ctr++;
-	// 					q.push({nr, nc});
-	// 				}
-	// 			}
-	// 		}
-
-	// 		// cerr << n << " " << m << " " << ctr << '\n';
-	// 		// assert(ctr == 3);
-	// 	}
-	// }
-
-	cout << "YES\n";
-	for (int i = 1; i <= n; ++i)
+	bool swp = false;
+	if (n % 3)
 	{
-		for (int j = 1; j <= m; ++j)
+		swp = true;
+		swap(n, m);
+	}
+
+	bool ans = true;
+	const auto func = [&](const auto &self, int r1, int c1, int r2, int c2) -> void
+	{
+		int r = r2 - r1 + 1, c = c2 - c1 + 1;
+		if (c % 2 == 0)
 		{
-			if (!('A' <= res[i][j] && res[i][j] <= 'Z'))
-			{
-				cerr << n << " and " << m << '\n';
-				assert(0);
-			}
-			cout << res[i][j];
+			place(r1, c1, r2, c2);
 		}
-		cout << '\n';
+		else if (r % 2 == 0)
+		{
+			place(r1, c1, r2, c1 + 2);
+			if (c2 > c1 + 2)
+				place(r1, c1 + 3, r2, c2);
+		}
+		else if (n >= 9 && m >= 5)
+		{
+			place9(1, 1, 9, 5);
+			if (n > 9)
+			{
+				self(self, 10, 1, n, 5);
+			}
+			if (m > 5)
+			{
+				self(self, 1, 6, 9, m);
+			}
+			if (n > 9 && m > 5)
+			{
+				self(self, 10, 6, n, m);
+			}
+		}
+		else
+		{
+			ans = false;
+		}
+	};
+	func(func, 1, 1, n, m);
+
+	cout << (ans ? "YES\n" : "NO\n");
+	if (!ans)
+		return;
+
+	if (swp)
+	{
+		for (int i = 1; i <= m; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+				cout << res[j][i];
+			cout << '\n';
+		}
+	}
+	else
+	{
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = 1; j <= m; ++j)
+				cout << res[i][j];
+			cout << '\n';
+		}
 	}
 }
-
+ 
 int main()
 {
 	fastio;
-
+ 
 	int TC = 1;
 	cin >> TC;
-
+ 
 	while (TC--)
 		solve();
-
+ 
 	return 0;
 }
--- Additional Problems/New Flight Routes.cpp
+++ Additional Problems/New Flight Routes.cpp
@@ -20,8 +20,8 @@ const int MAX = 1e5 + 69;
 vector<ii> ans;
 
 int N, M;
-int ds[MAX], ind[MAX];
-vector<int> adj[MAX], adjR[MAX], adjScc[MAX];
+int ds[MAX], ind[MAX], outd[MAX];
+vector<int> adj[MAX], adjR[MAX];
  
 inline int frep(int x) { return ds[x] == x ? x : ds[x] = frep(ds[x]); }
 inline void join(int x, int y) { ds[frep(y)] = frep(x); }
@@ -46,34 +46,87 @@ inline void dfs2(int node)
 	comp.pb(node);
 }
 
-// straighten the graph into a single in and out
-inline ii func(vector<int> nodes)
-{
-	deque <int> q;
-	vector<int> in0, out0;
-	for (int x : nodes) if (ind[x] == 0)
-		q.pb(x), in0.pb(x);
+struct Dinic {
+    struct FlowEdge {
+        int v, u;
+        long long cap, flow = 0;
+        FlowEdge(int V, int U, long long Cap) : v(V), u(U), cap(Cap) {}
+    };
 
-	while (!q.empty())
-	{
-		int node = q.front(); q.pop_front();
- 
-		if (adjScc[node].empty())
-			out0.pb(node);
- 
-		for (int to : adjScc[node])
-		{
-			ind[to]--;
-			if (ind[to] == 0)
-				q.push_front(to);
-		}
-	}
- 
-	int szin = (int)in0.size(), szout = (int)out0.size();
-	for (int i = 1; i < max(szin, szout); ++i)
-		ans.pb({out0[(i+1) % szout], in0[i % szin]});
-	return { in0[0], out0[1 % szout] };
-}
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int N, int S, int T) : n(N), s(S), t(T) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+
+    void add_edge(int v, int u, long long cap) {
+        edges.emplace_back(v, u, cap);
+        edges.emplace_back(u, v, 0);
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id : adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+                break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
  
 void solve()
 {
@@ -106,36 +159,30 @@ void solve()
 		}
 	}
  	
- 	cerr << '\n';
+	set<ii> sccEdges;
+	Dinic dinic(N+2, 0, N+1);
 	for (auto &[u, v] : edges) if (frep(u) != frep(v))
 	{
-		cerr << frep(u) << " " << frep(v) << '\n';
-		adjScc[frep(u)].pb(frep(v)), ind[frep(v)]++;
+		if (sccEdges.count({frep(u), frep(v)}))
+			continue;
+		sccEdges.insert({frep(u), frep(v)});
+		dinic.add_edge(frep(u), frep(v), 1);
+
+		outd[frep(u)]++;
+		ind[frep(v)]++;
 	}
-	return;
 
-	iota(ds, ds + MAX, 0);
-	for (auto &[u, v] : edges)
-		join(u, v);
-
-	vector<int> nodes[MAX];
-	for (int i = 1; i <= N; ++i)
-		nodes[frep(i)].pb(i);
-
-	vector<ii> lines;
-	for (int i = 1; i <= N; ++i) if (!nodes[i].empty())
+	for (int i = 1; i <= N; ++i) if (i == frep(i))
 	{
-		lines.pb(func(nodes[i]));
+		if (outd[i] == 0) dinic.add_edge(i, N+1, 1);
+		if (ind[i] == 0) dinic.add_edge(0, i, 1);
 	}
 
-	int sz = (int)lines.size();
-	for (int i = 0; i < sz; ++i)
-		ans.pb({lines[i].s2, lines[(i+1) % sz].f1});
+	cout << dinic.flow() << '\n';
 
-	cout << ans.size() << '\n';
-	for (auto [x, y] : ans)
-		cout << x << " " << y << '\n';
+
 	return;
+
 }
  
 int main()
